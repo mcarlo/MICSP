a82e0b187a99a3ded3125751d75ce8d5f94fbf4b
mcarlo <tom.anichini@gmail.com>
2014-06-23 20:40:05 -0700

    Removed junk

--- ProbSet2.2b.py
+++
@@ -1,8 +0,0 @@
-def payment(balance, annualInterestRate):
-    rate = annualInterestRate/12.0
-    return balance/(1 + (1 - pow(1/(1+rate),11))/rate)
-
-float_payment = payment(balance, annualInterestRate)
-payment = 10*(float_payment % 10 > 0.00) + int(float_payment/10)*10
-
-print "Lowest Payment: " + str(payment)
